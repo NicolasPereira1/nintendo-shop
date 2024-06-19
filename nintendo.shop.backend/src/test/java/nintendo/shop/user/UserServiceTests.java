24e8642d455861477bcf289973307c54d9e15971
NicolasPereira1 <nicolas_pereira27@hotmail.com>
2024-06-18 20:16:39 -0400

    fix(UserServicesTests): change expected result

--- nintendo.shop.backend/src/test/java/nintendo/shop/user/UserServiceTests.java
+++ nintendo.shop.backend/src/test/java/nintendo/shop/user/UserServiceTests.java
@@ -47,6 +47,6 @@ public class UserServiceTests {
         when(passwordEncoder.encode(PASSWORD)).thenReturn(PASSWORD);
         when(userRepository.save(any())).thenReturn(null);
 
-        assertEquals("Successfully registered!", userService.register(dto));
+        assertEquals("Successfully regitered!", userService.register(dto));
     }
 }
